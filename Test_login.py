a7df11fa140911eca1924bed192f2228b49ffbe5
abykov <abykov@htc-cs.ru>
2018-06-05 14:28:01 +0400

    add utility methods

--- Test_login.py
+++ Test_login.py
@@ -9,14 +9,19 @@ from selenium_fixture import driver
 import unittest, time, re
 
 
+def logout(driver):
+    driver.find_element_by_link_text("Выйти").click()
+
+def login(driver):
+    driver.find_element_by_id("user_email").click()
+    driver.find_element_by_id("user_email").clear()
+    driver.find_element_by_id("user_email").send_keys("test10@test.ru")
+    driver.find_element_by_id("user_password").click()
+    driver.find_element_by_id("user_password").clear()
+    driver.find_element_by_id("user_password").send_keys("tester10")
+    driver.find_element_by_name("commit").click()
 
 def test_auto(driver):
-        driver.get("http://kuki.webtest2.htc-cs.com/" + "/users/login")
-        driver.find_element_by_id("user_email").click()
-        driver.find_element_by_id("user_email").clear()
-        driver.find_element_by_id("user_email").send_keys("test10@test.ru")
-        driver.find_element_by_id("user_password").click()
-        driver.find_element_by_id("user_password").clear()
-        driver.find_element_by_id("user_password").send_keys("tester10")
-        driver.find_element_by_name("commit").click()
-    
+    driver.get("http://kuki.webtest2.htc-cs.com/" + "/users/login")
+    login(driver)
+    logout(driver)
