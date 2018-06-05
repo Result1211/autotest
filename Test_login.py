8c1a45e5d4ebfb1c18f26a1f5edf53a2820cc86b
abykov <abykov@htc-cs.ru>
2018-06-05 16:44:44 +0400

    replace utility methods to app

--- Test_login.py
+++ Test_login.py
@@ -5,20 +5,7 @@ from selenium.common.exceptions import *
 from selenium_fixture import app
 
 
-def logout(driver):
-    driver.find_element_by_link_text("Выйти").click()
-
-def login(driver, user):
-    driver.find_element_by_id("user_email").click()
-    driver.find_element_by_id("user_email").clear()
-    driver.find_element_by_id("user_email").send_keys(user.user_email)
-    driver.find_element_by_id("user_password").click()
-    driver.find_element_by_id("user_password").clear()
-    driver.find_element_by_id("user_password").send_keys(user.user_password)
-    driver.find_element_by_name("commit").click()
-
-
 def test_auto(app):
-    app.driver.get("http://kuki.webtest2.htc-cs.com/" + "/users/login")
-    login(app.driver, User.Admin())
-    logout(app.driver)
+    app.go_to_home_page()
+    app.login(User.Admin())
+    app.logout()
--- model/application.py
+++ model/application.py
@@ -1,4 +1,22 @@
+# -*- coding: utf-8 -*-
 class Application(object):
 
     def __init__(self, driver):
         self.driver = driver
+
+    def go_to_home_page(self):
+        self.driver.get("http://kuki.webtest2.htc-cs.com/"+"users/login")
+
+    def logout(self):
+        driver = self.driver
+        driver.find_element_by_link_text("Выйти").click()
+
+    def login(self, user):
+        driver = self.driver
+        driver.find_element_by_id("user_email").click()
+        driver.find_element_by_id("user_email").clear()
+        driver.find_element_by_id("user_email").send_keys(user.user_email)
+        driver.find_element_by_id("user_password").click()
+        driver.find_element_by_id("user_password").clear()
+        driver.find_element_by_id("user_password").send_keys(user.user_password)
+        driver.find_element_by_name("commit").click()
