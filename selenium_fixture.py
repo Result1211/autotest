084eb3f614cfd01c1037bac9868eafbc73b664b3
abykov <abykov@htc-cs.ru>
2018-06-06 14:57:56 +0400

    labuda
    This reverts commit 0d28f7e42de9883b036d3ac7b1ec7bcfd1b50539.

--- selenium_fixture.py
+++ selenium_fixture.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import pytest
 from model.application import Application
 
+<<<<<<< HEAD
 
 @pytest.fixture(scope="module")
 def app(request, browser_type, base_url):
@@ -10,6 +11,12 @@ def app(request, browser_type, base_url):
     elif browser_type == "firefox":
         driver = webdriver.FireFox("C:\FF_driver\geckodriver.exe")
    # driver.implicitly_wait(30)
+=======
+@pytest.fixture
+def app(request):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
+>>>>>>> parent of 0d28f7e... add asserts (login, logout)
     request.addfinalizer(driver.quit)
     return Application(driver, base_url)
 
