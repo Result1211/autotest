70566bd6c9913e868506ecc17de66c4c92250cb7
abykov <abykov@htc-cs.ru>
2018-06-06 15:09:09 +0400

    ready2

--- selenium_fixture.py
+++ selenium_fixture.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import pytest
 from model.application import Application
 
+<<<<<<< HEAD
 
 @pytest.fixture(scope="module")
 def app(request, browser_type, base_url):
@@ -20,6 +21,9 @@ def app(request):
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
 
+=======
+@pytest.fixture
+>>>>>>> parent of 0d28f7e... add asserts (login, logout)
 def app(request):
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
