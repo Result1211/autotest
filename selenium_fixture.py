6b39e43f11084435f3f526b160f2adaf79d3fc8c
abykov <abykov@htc-cs.ru>
2018-06-06 15:02:45 +0400

    Revert "add conftest"
    This reverts commit dfa8c79a5777a5ae9dd1136ffb1d66487c3ab915.

--- selenium_fixture.py
+++ selenium_fixture.py
@@ -2,27 +2,27 @@ from selenium import webdriver
 import pytest
 from model.application import Application
 
-<<<<<<< HEAD
 
 @pytest.fixture(scope="module")
-<<<<<<< HEAD
 def app(request, browser_type, base_url):
     if browser_type == "Chrome":
         driver = webdriver.Chrome()
     elif browser_type == "firefox":
         driver = webdriver.FireFox("C:\FF_driver\geckodriver.exe")
    # driver.implicitly_wait(30)
-=======
+   
 @pytest.fixture
 def app(request):
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
->>>>>>> parent of 0d28f7e... add asserts (login, logout)
-=======
+
 def app(request):
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
->>>>>>> parent of dfa8c79... add conftest
+
+def app(request):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
     request.addfinalizer(driver.quit)
     return Application(driver)
 
