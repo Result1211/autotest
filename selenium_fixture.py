dfa8c79a5777a5ae9dd1136ffb1d66487c3ab915
abykov <abykov@htc-cs.ru>
2018-06-06 14:32:59 +0400

    add conftest

--- selenium_fixture.py
+++ selenium_fixture.py
@@ -4,9 +4,12 @@ from model.application import Application
 
 
 @pytest.fixture(scope="module")
-def app(request):
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(30)
+def app(request, browser_type, base_url):
+    if browser_type == "Chrome":
+        driver = webdriver.Chrome()
+    elif browser_type == "firefox"
+        driver == webdriver.FireFox ("C:\FF_driver\geckodriver.exe")
+   # driver.implicitly_wait(30)
     request.addfinalizer(driver.quit)
-    return Application(driver)
+    return Application(driver, base_url)
 
